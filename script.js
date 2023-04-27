57b171091f5f29ac3cd949ba04197bfde4f4cac0
Harshvardhan Singh Jadon <jadonharsh109@gmail.com>
2023-04-27 16:00:35 +0530

    Navbar Fixed

--- script.js
+++
@@ -1,6 +0,0 @@
-let l1 = document.getElementById('li1');
-let l2 = document.getElementById('li2');
-let l3 = document.getElementById('li3');
-let l4 = document.getElementById('li4');
-let l5 = document.getElementById('li5');
-let l6 = document.getElementById('li6');
